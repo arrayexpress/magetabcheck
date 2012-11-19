96d13de76dc04cf2437ae6c9a663fdaa8518ec63
olgamelnichuk <olkin@ebi.ac.uk>
2012-11-19 15:50:04 +0000

    an sdrf global check added..

---
+++ src/main/java/uk/ac/ebi/fg/annotare2/magetab/checks/sdrf/ListOfLabeledExtractNodesMustBeEmpty.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2012 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or impl
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.ebi.fg.annotare2.magetab.checks.sdrf;
+
+import uk.ac.ebi.fg.annotare2.magetab.checker.MageTabCheck;
+import uk.ac.ebi.fg.annotare2.magetab.checks.EmptyListCheck;
+import uk.ac.ebi.fg.annotare2.magetab.model.sdrf.SdrfLabeledExtractNode;
+
+import static uk.ac.ebi.fg.annotare2.magetab.checker.CheckApplicationType.HTS_ONLY;
+
+/**
+ * @author Olga Melnichuk
+ */
+@MageTabCheck(value = "There are must not be any Labeled Extract nodes in HTS experiments", application = HTS_ONLY)
+public class ListOfLabeledExtractNodesMustBeEmpty extends EmptyListCheck<SdrfLabeledExtractNode>{
+}
