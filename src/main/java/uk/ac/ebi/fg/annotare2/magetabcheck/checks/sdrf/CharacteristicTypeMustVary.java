ca297d3274c32621f1a7a27be7b0c647f7a97a19
arrayexpress <arrayexpress@ebi.ac.uk>
2014-08-22 11:16:30 +0100

    Improved detection of duplicate characteristics

--- src/main/java/uk/ac/ebi/fg/annotare2/magetabcheck/checks/sdrf/CharacteristicTypeMustVary.java
+++ src/main/java/uk/ac/ebi/fg/annotare2/magetabcheck/checks/sdrf/CharacteristicTypeMustVary.java
@@ -6,12 +6,11 @@ import uk.ac.ebi.fg.annotare2.magetabcheck.checker.annotation.Visit;
 import uk.ac.ebi.fg.annotare2.magetabcheck.model.sdrf.SdrfCharacteristicAttribute;
 import uk.ac.ebi.fg.annotare2.magetabcheck.model.sdrf.SdrfSourceNode;
 
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Created by emma on 21/08/2014.
@@ -24,27 +23,26 @@ import static org.hamcrest.Matchers.equalTo;
 )
 public class CharacteristicTypeMustVary {
 
-    Collection<SdrfCharacteristicAttribute> characteristics;
+    boolean wereDuplicatesSpotted = false;
 
     @Visit
-    // Get our list of characteristics
     public void visit(SdrfSourceNode sourceNode) {
-        characteristics = sourceNode.getCharacteristics();
+        if (!wereDuplicatesSpotted) {
+            Set<String> usedAttributes = new HashSet<String>();
+            for (SdrfCharacteristicAttribute attr : sourceNode.getCharacteristics()) {
+                String attrType = attr.getType().toLowerCase();
+                if (usedAttributes.contains(attrType)) {
+                    wereDuplicatesSpotted = true;
+                    break;
+                }
+                usedAttributes.add(attrType);
+            }
+        }
     }
+
 
     @Check
     public void check() {
-        check(characteristics);
+        assertThat(wereDuplicatesSpotted, is(false));
     }
-
-    // Logic is to compare number of characteristic types with a unique set of types
-    private void check(Collection<SdrfCharacteristicAttribute> characteristics) {
-
-        Set<String> uniques = new HashSet<String>();
-        for (SdrfCharacteristicAttribute attr : characteristics) {
-            uniques.add(attr.getType().toLowerCase());
-        }
-        assertThat(uniques.size(), equalTo(characteristics.size()));
-    }
-
 }
