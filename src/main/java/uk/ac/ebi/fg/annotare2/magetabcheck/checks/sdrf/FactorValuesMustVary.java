a22c90897eaf063e4fce0d29a9b394ce36ec7c15
arrayexpress <arrayexpress@ebi.ac.uk>
2015-01-19 14:34:12 +0000

    Fixes misfiring "Factor Values Must Vary" check when there is just one sample

--- src/main/java/uk/ac/ebi/fg/annotare2/magetabcheck/checks/sdrf/FactorValuesMustVary.java
+++ src/main/java/uk/ac/ebi/fg/annotare2/magetabcheck/checks/sdrf/FactorValuesMustVary.java
@@ -6,13 +6,13 @@ import com.google.common.collect.Sets;
 import uk.ac.ebi.fg.annotare2.magetabcheck.checker.annotation.Check;
 import uk.ac.ebi.fg.annotare2.magetabcheck.checker.annotation.MageTabCheck;
 import uk.ac.ebi.fg.annotare2.magetabcheck.checker.annotation.Visit;
-import uk.ac.ebi.fg.annotare2.magetabcheck.model.sdrf.SdrfAssayNode;
-import uk.ac.ebi.fg.annotare2.magetabcheck.model.sdrf.SdrfFactorValueAttribute;
-import uk.ac.ebi.fg.annotare2.magetabcheck.model.sdrf.SdrfScanNode;
+import uk.ac.ebi.fg.annotare2.magetabcheck.model.sdrf.*;
 
+import java.util.Iterator;
 import java.util.Set;
 import java.util.SortedSet;
 
+import static com.google.common.collect.Sets.newHashSet;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static uk.ac.ebi.fg.annotare2.magetabcheck.checker.CheckDynamicDetailSetter.setCheckDynamicDetail;
@@ -27,14 +27,17 @@ public class FactorValuesMustVary {
 
     private Multimap<String, String> assayFactorValues;
     private Multimap<String, String> scanFactorValues;
+    private Set<SdrfSourceNode> sourceNodes;
 
     public FactorValuesMustVary() {
         assayFactorValues = HashMultimap.create();
         scanFactorValues = HashMultimap.create();
+        sourceNodes = newHashSet();
     }
 
     @Visit
     public void visit(SdrfAssayNode assayNode) {
+        addSourceNodes(assayNode);
         for (SdrfFactorValueAttribute attr : assayNode.getFactorValues()) {
             addFactorValueAttribute(assayFactorValues, attr);
         }
@@ -42,6 +45,7 @@ public class FactorValuesMustVary {
 
     @Visit
     public void visit(SdrfScanNode scanNode) {
+        addSourceNodes(scanNode);
         for (SdrfFactorValueAttribute attr : scanNode.getFactorValues()) {
             addFactorValueAttribute(scanFactorValues, attr);
         }
@@ -53,72 +57,86 @@ public class FactorValuesMustVary {
         check(scanFactorValues);
     }
 
+    private void addSourceNodes(SdrfGraphNode node) {
+        if (node instanceof SdrfSourceNode) {
+            sourceNodes.add((SdrfSourceNode)node);
+        } else {
+            for (SdrfGraphNode parent : node.getParentNodes()) {
+                addSourceNodes(parent);
+            }
+        }
+    }
+
     private void addFactorValueAttribute(Multimap<String, String> multimap, SdrfFactorValueAttribute attr) {
         String value = attr.getValue();
         multimap.put(attr.getType(), value == null ? "" : value.trim());
     }
 
     private void check(Multimap<String, String> multimap) {
-        Set factors = multimap.keySet();
-        SortedSet<String> offendingFactors = Sets.newTreeSet();
+        if (sourceNodes.size() > 1) {
+            Set factors = multimap.keySet();
+            SortedSet<String> offendingFactors = Sets.newTreeSet();
 
-        // Compound and dose are dependant factors
-        // If we have both only one needs to vary
-        // If we have only one then it needs to vary
-        int min = 0;
-        if (factors.contains("dose")) {
-           min++;
-        }
-        if (factors.contains("compound")) {
-            min++;
-        }
+            // Compound and dose are dependant factors
+            // If we have both only one needs to vary
+            // If we have only one then it needs to vary
+            int min = 0;
+            if (factors.contains("dose")) {
+                min++;
+            }
+            if (factors.contains("compound")) {
+                min++;
+            }
 
-        // Min determines if we have compound and dose, or just one
-        // Thus if we have both min equals 2 and we should have more unique values than min
-        // to ensure values vary. This also catches the case where we have just dose or just compound
-        if (factors.contains("compound") || factors.contains("dose")) {
+            // Min determines if we have compound and dose, or just one
+            // Thus if we have both min equals 2 and we should have more unique values than min
+            // to ensure values vary. This also catches the case where we have just dose or just compound
+            if (factors.contains("compound") || factors.contains("dose")) {
 
-            int numUniqueFactorValues = multimap.get("dose").size() + multimap.get("compound").size() ;
+                int numUniqueFactorValues = multimap.get("dose").size() + multimap.get("compound").size();
 
-            // if values of compound and dose do not vary add to offending factors and then assert at end
-            if (numUniqueFactorValues <= min) {
-                if (factors.contains("compound") && factors.contains("dose")) {
-                    offendingFactors.add("dose");
-                    offendingFactors.add("compound");
-                } else if (factors.contains("dose")) {
-                    offendingFactors.add("dose");
-                } else {
-                    offendingFactors.add("compound");
+                // if values of compound and dose do not vary add to offending factors and then assert at end
+                if (numUniqueFactorValues <= min) {
+                    if (factors.contains("compound") && factors.contains("dose")) {
+                        offendingFactors.add("dose");
+                        offendingFactors.add("compound");
+                    } else if (factors.contains("dose")) {
+                        offendingFactors.add("dose");
+                    } else {
+                        offendingFactors.add("compound");
+                    }
+                }
+
+            }
+
+            // Check all factors other than compound and dose
+            // Store non-varying factors
+
+            for (String key : multimap.keySet()) {
+                if (!key.equals("dose") && !key.equals("compound")) {
+
+                    // If size is equal to one then the values of that factor do not vary
+                    if (multimap.get(key).size() == 1) {
+                        offendingFactors.add(key);
+                    }
                 }
             }
 
-        }
+            //  Check non-varying factors
+            if (!offendingFactors.isEmpty()) {
 
-        // Check all factors other than compound and dose
-        // Store non-varying factors
-
-        for (String key : multimap.keySet()) {
-            if (!key.equals("dose") && !key.equals("compound")) {
-
-                // If size is equal to one then the values of that factor do not vary
-                if (multimap.get(key).size() == 1) {
-                    offendingFactors.add(key);
+                // Format string that user sees
+                StringBuilder sb = new StringBuilder();
+                for (Iterator<String> it = offendingFactors.iterator(); it.hasNext(); ) {
+                    sb.append(it.next());
+                    if (it.hasNext()) {
+                        sb.append(", ");
+                    }
                 }
+                setCheckDynamicDetail("Offending factor" + (offendingFactors.size() > 1 ? "s" : "") + ": " + sb.toString());
             }
+            // Throws error if offendingFactors contains entries
+            assertThat(offendingFactors.size(), equalTo(0));
         }
-
-        //  Check non-varying factors
-        if (!offendingFactors.isEmpty()) {
-
-            // Format string that user sees
-            StringBuilder sb = new StringBuilder();
-            for (String factor : offendingFactors) {
-                sb.append(factor + ", ");
-            }
-            setCheckDynamicDetail("Offending factor(s): " + sb.toString());
-        }
-        // Throws error if offendingFactors contains entries
-        assertThat(offendingFactors.size(), equalTo(0));
-
     }
 }
