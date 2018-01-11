a61dce8b39f05a2136172266196b434fa16f3252
Haider Iqbal <haideri@ebi.ac.uk>
2018-01-11 15:58:29 +0000

    -Update check to allow blank processed files

--- src/main/java/uk/ac/ebi/fg/annotare2/magetabcheck/checks/sdrf/SdrfSimpleChecks.java
+++ src/main/java/uk/ac/ebi/fg/annotare2/magetabcheck/checks/sdrf/SdrfSimpleChecks.java
@@ -874,8 +874,7 @@ public class SdrfSimpleChecks {
     private static boolean checkFileName(SdrfDataNode dataNode){
         // We only want to accept files with alphanumeric characters, no spaces, symbols etc.
         String filename = dataNode.getName();
-        //return filename.matches("^[_a-zA-Z0-9\\-\\.]+$");
-        return true;
+        return filename.matches("^[_a-zA-Z0-9\\-\\.]+$");
     }
 
     private static boolean isValidPositiveInteger(Collection<String> values) {
